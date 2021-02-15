8e3b9e4cc6928f57973e19e8537f4b8eed06ea9e
Jacob Bare <jacob@parameter1.com>
2021-02-15 14:17:30 -0600

    Build types

--- packages/cli/src/constants.js
+++ packages/cli/src/constants.js
@@ -1,6 +1,7 @@
 const ROOT_API_PATH = 'apis/ads/publisher';
 
 const constants = Object.freeze({
+  DOCS_URI: 'https://developers.google.com/ad-manager/api/reference',
   ROOT_API_PATH,
   ROOT_URI: `https://ads.google.com/${ROOT_API_PATH}`,
 });
--- packages/cli/src/graphql-type.js
+++ packages/cli/src/graphql-type.js
@@ -1,7 +1,8 @@
-const loadWSDL = require('./load-wsdl');
-const typeFromURL = require('./type-from-url');
+const WSDL = require('./wsdl');
+const { DOCS_URI } = require('./constants');
 
 const typeMap = {
+  'xsd:boolean': 'Boolean',
   'xsd:double': 'Float',
   'xsd:long': 'BigInt',
   'xsd:string': 'String',
@@ -9,38 +10,95 @@ const typeMap = {
   DateTime: 'GAMDateTime',
 };
 
-const buildAttr = (field) => {
-  let type = typeMap[field.type];
+const createEnumName = (name) => `${name.replace(/\./g, '')}Enum`;
+
+const buildAttr = (field, referencedTypes, cleanDocs) => {
+  let type;
+  if (typeMap[field.type]) {
+    // explicitally mapped
+    type = typeMap[field.type];
+  } else {
+    // either an enum or another object type
+    const typeObj = referencedTypes.get(field.type);
+    if (!typeObj) throw new Error(`Unable to find a reference type for ${field.type}`);
+    type = typeObj.isEnumerated ? createEnumName(typeObj.name) : field.type;
+  }
   if (field.multiple) type = `[${type}]`;
   if (field.required) type = `${type}!`;
   const lines = [];
-  if (field.documentation) lines.push(`"${field.documentation}"`);
+  if (field.documentation) lines.push(`"${cleanDocs(field.documentation)}"`);
   lines.push(`${field.name}: ${type}`);
   return lines.map((line) => `  ${line}`).join('\n');
 };
 
-const buildAttrs = (fields) => {
-  const lines = fields
-    .toArray()
-    .filter((field) => typeMap[field.type])
-    .map((field) => buildAttr(field))
-    .join('\n');
-  return lines;
+/**
+ *
+ * @param {WSDLType} type
+ */
+const buildTypeDefinition = (type, referencedTypes, cleanDocs) => `
+"${cleanDocs(type.documentation)}"
+type ${type.name} {
+${type.fields.map((field) => buildAttr(field, referencedTypes, cleanDocs)).join('\n')}
+}
+`;
+
+/**
+ *
+ * @param {object} enumeration
+ */
+const buildEnumValue = ({ value, documentation }, cleanDocs) => {
+  const lines = [];
+  if (documentation) lines.push(`"${cleanDocs(documentation)}"`);
+  lines.push(value);
+  return lines.map((line) => `  ${line}`).join('\n');
 };
 
-const buildRootType = (type) => `
-
-"${type.documentation}"
-type ${type.name} {
-${buildAttrs(type.fields)}
+/**
+ *
+ * @param {WSDLType} type
+ */
+const buildEnumDefinition = (type, cleanDocs) => `
+"${cleanDocs(type.documentation)}"
+enum ${createEnumName(type.name)}Enum {
+${type.enumeration.map((enumer) => buildEnumValue(enumer, cleanDocs)).join('\n')}
 }
-
 `;
 
 module.exports = async ({ url } = {}) => {
-  const wsdl = await loadWSDL({ url });
-  const target = typeFromURL(url);
-  const primaryType = wsdl.getType(target, true);
-  if (!primaryType) throw new Error(`Unable to extract target type ${target} from ${url}`);
-  return buildRootType(primaryType);
+  const wsdl = await WSDL.loadFromUrl(url);
+  const primaryType = wsdl.getPrimaryType();
+  if (!primaryType) throw new Error(`Unable to extract target type ${wsdl.primaryTypeName} from ${url}`);
+
+  // Get all type definitions (but skip Date and DateTime... these will be treated as scalars)
+  const skip = ['Date', 'DateTime'];
+  const referencedTypes = wsdl.getAllReferencedTypesFor(primaryType.name);
+  const filteredTypes = referencedTypes.filter((type) => !skip.includes(type.name));
+
+  /**
+   * Replaces @code and @link references in documentation.
+   *
+   * @param {string} str
+   */
+  const cleanDocs = (str) => {
+    if (!str) return str;
+    return str
+      .replace(/\{@code (.+?)\}/g, '`$1`')
+      .replace(/`/g, '\\`')
+      .replace(/\{@link (.+?)\}/g, (_, link) => {
+        const parts = link.split(' ');
+        const linkTo = parts.shift();
+        const label = parts.join(' ') || linkTo;
+        const docsUrl = `${DOCS_URI}/${wsdl.version}/${wsdl.name}.${linkTo}`;
+        return `[${label}](${docsUrl})`;
+      });
+  };
+
+  return filteredTypes.reduce((o, type) => {
+    if (type.enumeration.length) {
+      o.enums.push(buildEnumDefinition(type, cleanDocs));
+      return o;
+    }
+    o.types.push(buildTypeDefinition(type, referencedTypes, cleanDocs));
+    return o;
+  }, { types: [], enums: [] });
 };
