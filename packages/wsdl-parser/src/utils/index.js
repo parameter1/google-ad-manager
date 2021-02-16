4c555f4a44cfeee7f55e100f0c08e347dafa1081
Jacob Bare <jacob@parameter1.com>
2021-02-16 10:01:29 -0600

    Add root utils export to wsdl package

---
+++ packages/wsdl-parser/src/utils/index.js
@@ -0,0 +1,9 @@
+const cleanDocs = require('./clean-docs');
+const cleanType = require('./clean-type');
+const serviceUrlInfo = require('./service-url-info');
+
+module.exports = {
+  cleanDocs,
+  cleanType,
+  serviceUrlInfo,
+};
