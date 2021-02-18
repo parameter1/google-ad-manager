25d06c52c428073ada6469614a4ce2fef8de67c8
Jacob Bare <jacob@parameter1.com>
2021-02-18 10:45:22 -0600

    Return built graphql files

--- packages/graphql-generator/src/build-all.js
+++ packages/graphql-generator/src/build-all.js
@@ -1,3 +1,4 @@
+const { camelize, underscore } = require('inflected');
 const build = require('./build-one');
 
 module.exports = async ({ urls } = {}) => {
@@ -15,6 +16,7 @@ module.exports = async ({ urls } = {}) => {
       result[key].forEach(({ hash, contents }, name) => {
         const loaded = map.get(name);
         const toSet = {
+          key,
           target: loaded && loaded.count > 1 ? 'common.js' : result.filename,
           count: loaded ? loaded.count + 1 : 1,
           hashes: loaded ? loaded.hashes.add(hash) : new Set([hash]),
@@ -24,5 +26,47 @@ module.exports = async ({ urls } = {}) => {
       });
     });
   });
-  return results;
+
+  const itemMap = new Map();
+  map.forEach(({ target, contents }) => {
+    const items = itemMap.has(target) ? itemMap.get(target) : [];
+    items.push(contents);
+    itemMap.set(target, items);
+  });
+
+  const fileMap = new Map();
+  let filenames = [];
+  itemMap.forEach((items, filename) => {
+    items.unshift(
+      '// THIS FILE IS GENERATED. DO NOT EDIT.',
+      "const { gql } = require('apollo-server-express');",
+      'module.exports = gql`',
+    );
+    items.push('`;');
+    fileMap.set(filename, `${items.join('\n\n')}\n`);
+    const file = filename.replace('.js', '');
+    filenames.push({ filename: file, varName: camelize(underscore(file), false) });
+  });
+
+  filenames = filenames.sort((a, b) => {
+    if (a.filename > b.filename) return 1;
+    if (a.filename < b.filename) return -1;
+    return 0;
+  });
+  const index = [];
+  index.push(
+    '// THIS FILE IS GENREATED. DO NOT EDIT.',
+    "const { gql } = require('apollo-server-express');\n",
+    "const globalDefinitions = require('../../../definitions')",
+  );
+
+  index.push(...filenames.map(({ filename, varName }) => `const ${varName} = require('./${filename}');`));
+  index.push('\nmodule.exports = gql`\n');
+  index.push('${globalDefinitions}'); // eslint-disable-line
+  index.push(...filenames.map(({ varName }) => `\${${varName}}`));
+  index.push('\n`;');
+
+  fileMap.set('index.js', `${index.join('\n')}\n`);
+
+  return fileMap;
 };
