effcc79b7d815842448729149dc6203c11f9d5ce
Jacob Bare <jacob@parameter1.com>
2021-02-25 09:04:24 -0600

    Disable new relic by default

--- services/graphql/src/env.js
+++ services/graphql/src/env.js
@@ -9,8 +9,8 @@ const {
 module.exports = cleanEnv(process.env, {
   HOST: str({ desc: 'The host that the service will run on.', default: '0.0.0.0' }),
   JSON_KEY_FILE_PATH: str({ desc: 'An absolute path to the Google Service account JSON file.' }),
-  NEW_RELIC_ENABLED: bool({ desc: 'Whether New Relic is enabled.', default: true, devDefault: false }),
-  NEW_RELIC_LICENSE_KEY: str({ desc: 'The license key for New Relic.', devDefault: '(unset)' }),
+  NEW_RELIC_ENABLED: bool({ desc: 'Whether New Relic is enabled.', default: false }),
+  NEW_RELIC_LICENSE_KEY: str({ desc: 'The license key for New Relic.', default: '(unset)' }),
   NETWORK_CODE: num({ desc: 'The Google Ad Manager network code to access.' }),
   PORT: port({ desc: 'The port that the service will run on.', default: 80 }),
   VERSION: str({ desc: 'The Google Ad Manager API version to use.' }),
